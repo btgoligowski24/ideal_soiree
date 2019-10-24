8e251ed197224d1c2c72829ee7cc75404ab40258
Magus <mfail792@gmail.com>
2019-10-24 00:39:10 -0500

    started api routing for DB

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,67 @@
 var db = require("../models");
 
 module.exports = function(app) {
-    // Get all examples
-    app.get("/api/examples", function(req, res) {
-        db.Example.findAll({}).then(function(dbExamples) {
-            res.json(dbExamples);
-        });
+  // Get all of the themes
+  app.get("/api/themes", function(req, res) {
+    db.themes_db.findAll({}).then(function(themes_db) {
+      res.json(themes_db);
     });
+  });
 
-    // Create a new example
-    app.post("/api/examples", function(req, res) {
-        db.Example.create(req.body).then(function(dbExample) {
-            res.json(dbExample);
-        });
-    });
+  // Post route for saving a new food
+  app.post("/api/food", function(req, res) {
+    db.food
+      .create({
+        text: req.body.text,
+        complete: req.body.complete
+      })
+      .then(function(food) {
+        res.json(food);
+      })
+      .catch(function(err) {
+        // Whenever a validation or flag fails, an error is thrown
+        res.json(err);
+      });
+  });
 
-    // Delete an example by id
-    app.delete("/api/examples/:id", function(req, res) {
-        db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-            res.json(dbExample);
-        });
-    });
+  //Post route for saving a new drink
+  app.post("/api/drinks", function(req, res) {
+    db.drinks
+      .create({
+        text: req.body.text,
+        complete: req.body.complete
+      })
+      .then(function(drinks) {
+        res.json(drinks);
+      })
+      .catch(function(err) {
+        // Whenever a validation or flag fails, an error is thrown
+        res.json(err);
+      });
+  });
+
+  //Post route for saving predefined theme to DB
+  app.post("/api/food", function(req, res) {
+    db.predefines
+      .create({
+        text: req.body.text,
+        complete: req.body.complete
+      })
+      .then(function(predefines) {
+        res.json(predefines);
+      })
+      .catch(function(err) {
+        // Whenever a validation or flag fails, an error is thrown
+        res.json(err);
+      });
+  });
+
+  // Delete an example by id
+  app.delete("/api/themes/:id", function(req, res) {
+    db.themes_db
+      .destroy({ where: { id: req.params.id } })
+      .then(function(themes_db) {
+        res.json(themes_db);
+      });
+  });
 };
