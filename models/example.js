26c2e6f8854e3e28225b6ca438c78c4365ce1b5d
Magus <mfail792@gmail.com>
2019-10-24 15:50:59 -0500

    added theme model including food, name and drink category, shortened schema code for DB reference

--- models/example.js
+++ models/example.js
@@ -1,7 +1,29 @@
-module.exports = function(sequelize, DataTypes) {
-    var Example = sequelize.define("Example", {
-        text: DataTypes.STRING,
-        description: DataTypes.TEXT
-    });
-    return Example;
-};
+//referencing connection to library
+var Sequelize = require("sequelize");
+// sequelize (lowercase) references our connection to the DB.
+var sequelize = require("../config/connection.js");
+
+// Creates a "theme" model that matches up with DB
+var Theme = sequelize.define(
+  "Theme",
+  {
+    // the routeName gets saved as a string
+    routeName: Sequelize.STRING,
+    // the name of the theme (a string)
+    name: Sequelize.STRING,
+    // the theme's drink (a string)
+    drinks: Sequelize.STRING,
+    // the theme's meal (a string)
+    food: Sequelize.STRING
+  },
+  {
+    // disables the modification of tablenames
+    freezeTableName: true
+  }
+);
+
+// Syncs with DB
+Theme.sync();
+
+// Makes the Theme Model available for other files (will also create a table)
+module.exports = Theme;
