b50d039cb9052ae90ffb2dd51d01ae6352e6b70e
Magus <mfail792@gmail.com>
2019-10-24 00:19:11 -0500

    started htmlroutes to diff pages referencing DB (see notes at bottom), started example.js, updated schema to reference themes DB

--- models/example.js
+++ models/example.js
@@ -1,7 +1,21 @@
 module.exports = function(sequelize, DataTypes) {
-    var Example = sequelize.define("Example", {
-        text: DataTypes.STRING,
-        description: DataTypes.TEXT
-    });
-    return Example;
+  var Theme = sequelize.define("Theme", {
+    text: {
+      type: DataTypes.STRING,
+      // AllowNull is a flag that restricts a theme from being entered if it doesn't
+      // have a text value
+      allowNull: false,
+      // len is a validation that checks that our theme is between 1 and 140 characters
+      validate: {
+        len: [1, 140]
+      }
+    },
+    complete: {
+      type: DataTypes.BOOLEAN,
+      // defaultValue is a flag that defaults a new theme complete value to false if
+      // it isn't supplied one
+      defaultValue: false
+    }
+  });
+  return Theme;
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,40 +1,30 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
+  //load build a theme page data from themes table
+  module.exports = app => {
+    app.get("/build", (req, res) => {
+      db.themes.findAll({}).then(function(data) {
+        const handlebarsObj = {
+          theme: data
+        };
+        res.render("build", handlebarsObj);
       });
     });
-  });
-
-  //Load for build page
-  app.get("/build", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("build", {
-        title: "Build Your Own Theme!",
-        style: "build.css",
-        description: "Here you can build your own theme"
-      });
+  };
+  //load index page and list of predefined themes from predefines table
+  app.get("/index", (req, res) => {
+    db.themes_name.findAll({}).then(function(data) {
+      const handlebarsObj = {
+        themes_name: data
+      };
+      res.render("index", handlebarsObj);
     });
-  });
-
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
-
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
   });
 };
+
+// might need to use below setup instead for routing to each different handlebar site (main, index and build):
+
+// app.get("/", function(req, res) {
+//   res.sendFile(path.join(__dirname, "../views/index.handlebars"));
+// });
