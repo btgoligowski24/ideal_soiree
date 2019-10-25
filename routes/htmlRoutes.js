2bb646a38cd1e59081517f9104c63fc7a9df6343
Nicole Marty <nmarty310@gmail.com>
2019-10-25 10:06:39 -0500

    commenting out the database stuff until we have all the routing and models situated. shoudn't get that findALL error anymore and pages should work as they did.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,49 +1,46 @@
 var db = require("../models");
 
 module.exports = function(app) {
+
     // Load index page
     app.get("/", function(req, res) {
-        db.Example.findAll({}).then(function(dbExamples) {
-            res.render("index", {
-                style: "styles.css",
-                msg: "Welcome!",
-                examples: dbExamples
-            });
+        //db.Themes.findAll({}).then(function(themes_db) {
+        res.render("index", {
+            style: "styles.css"
         });
     });
+    //});
 
     //Load for build page
     app.get("/build", function(req, res) {
-        db.Example.findAll({}).then(function(dbExamples) {
-            res.render("build", {
-                title: "Build Your Own Theme!",
-                style: "build.css",
-                description: "Here you can build your own theme"
-            });
+        //db.Themes.findAll({}).then(function(themes_db) {
+        res.render("build", {
+            title: "Build Your Own Theme!",
+            style: "build.css",
+            description: "Here you can build your own theme"
         });
     });
+    //});
 
     //Load for themes page
     app.get("/themes", function(req, res) {
-        db.Example.findAll({}).then(function(dbExamples) {
-            res.render("themes", {
-                title: "Starter Themes",
-                style: "themes.css",
-                description: "See our predefined themes."
-            });
+        //  db.Themes.findAll({}).then(function(themes_db) {
+        res.render("themes", {
+            title: "Starter Themes",
+            style: "themes.css",
+            description: "See our predefined themes."
         });
     });
+    //});
 
     // Load example page and pass in an example by id
     app.get("/examples/:id", function(req, res) {
-        db.Example.findOne({ where: { id: req.params.id } }).then(function(
-            dbExample
-        ) {
-            res.render("example", {
-                example: dbExample
-            });
+        //db.Themes.findOne({ where: { id: req.params.id } }).then(function(themes_db) {
+        res.render("example", {
+            example: themes_db
         });
     });
+    //});
 
     // Render 404 page for any unmatched routes
     app.get("*", function(req, res) {
