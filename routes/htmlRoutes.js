62f3eced121879b885ea34dcef066955df67894f
Nicole Marty <nmarty310@gmail.com>
2019-10-24 19:16:38 -0500

    link connecting homepage to themes page working (created html route and made seperate css file for themes so didn't have to mess with bg image on the homepage

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,41 +1,52 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        style: "styles.css",
-        msg: "Welcome!",
-        examples: dbExamples
-      });
+    // Load index page
+    app.get("/", function(req, res) {
+        db.Example.findAll({}).then(function(dbExamples) {
+            res.render("index", {
+                style: "styles.css",
+                msg: "Welcome!",
+                examples: dbExamples
+            });
+        });
     });
-  });
 
-  //Load for build page
-  app.get("/build", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("build", {
-        title: "Build Your Own Theme!",
-        style: "build.css",
-        description: "Here you can build your own theme"
-      });
+    //Load for build page
+    app.get("/build", function(req, res) {
+        db.Example.findAll({}).then(function(dbExamples) {
+            res.render("build", {
+                title: "Build Your Own Theme!",
+                style: "build.css",
+                description: "Here you can build your own theme"
+            });
+        });
     });
-  });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.render("example", {
-        example: dbExample
-      });
+    //Load for themes page
+    app.get("/themes", function(req, res) {
+        db.Example.findAll({}).then(function(dbExamples) {
+            res.render("themes", {
+                title: "Starter Themes",
+                style: "themes.css",
+                description: "See our predefined themes."
+            });
+        });
     });
-  });
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
-  });
+    // Load example page and pass in an example by id
+    app.get("/examples/:id", function(req, res) {
+        db.Example.findOne({ where: { id: req.params.id } }).then(function(
+            dbExample
+        ) {
+            res.render("example", {
+                example: dbExample
+            });
+        });
+    });
+
+    // Render 404 page for any unmatched routes
+    app.get("*", function(req, res) {
+        res.render("404");
+    });
 };
