320ca68404e48ed4fb54b2234c13599af5b44f56
Ben Goligowski <51453763+btgoligowski24@users.noreply.github.com>
2019-10-25 17:48:19 -0500

    adding a theme works

--- models/themes.js
+++ models/themes.js
@@ -21,27 +21,3 @@ module.exports = function(sequelize, DataTypes) {
 
   return Theme;
 };
-
-// Creates a "foodDrink" table that matches up with DB
-module.exports = function(sequelize, DataTypes) {
-  var foodDrink = sequelize.define("foodDrink", {
-    // Giving the Meal table a name of type STRING
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
-    }
-  });
-
-  foodDrink.associate = function(models) {
-    // Associating Theme with Posts
-    // When an Theme is deleted, also delete any associated Posts
-    foodDrink.hasMany(models.foodDrink, {
-      onDelete: "cascade"
-    });
-  };
-
-  return foodDrink;
-};
