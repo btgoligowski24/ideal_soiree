385619277f9e12d760dd9324405354ab070608d4
Nicole Marty <nmarty310@gmail.com>
2019-10-27 20:22:55 -0500

    update themes.handlebars

---
+++ models/seeds.sql
@@ -0,0 +1,43 @@
+USE themes_db;
+
+INSERT INTO themes (name, description, createdAt, updatedAt)
+VALUES ("Almost Famous", "Embrace freedom, love, and rock & roll", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO themes (name, description, createdAt, updatedAt) 
+VALUES ("Alice in Wonderland", "Take a journey down the rabbit hole for a mostly guarenteed good time.", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO themes (name, description, createdAt, updatedAt) 
+VALUES ("Sex and the City", "OMG BESTIES FOREVERRRR! *SQUEALS AND GROUP HUG*", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO themes (name, description, createdAt, updatedAt) 
+VALUES ("Fright Fest", "It'll be one hell of a hauntingly frightening night...", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO themes (name, description, createdAt, updatedAt) 
+VALUES ("Winter Wonderland", "Celebrate the season with friends and family", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO themes (name, description, createdAt, updatedAt) 
+VALUES ("Cinco de Drinko", "One tequila, two tequila, three tequila, floor.", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO themes (name, description, createdAt, updatedAt) 
+VALUES ("Prohibition", "Fight for your right to parrrtayyyy!", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO themes (name, description, createdAt, updatedAt) 
+VALUES ("A Night in Paris", "On se voit a paris?", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO themes (name, description, createdAt, updatedAt) 
+VALUES ("Impeachment Party", "About damn time.", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO themes (name, description, createdAt, updatedAt) 
+VALUES ("Redneck Yacht Club", "Featuring an appearance by recently impeached Donald Trump", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO fooddrinks (name, imageURL, isMeal, apiID, ingredients, createdAt, updatedAt, ThemeId)
+VALUES ("Almost Famous", "Embrace freedom, love, and rock & roll", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO fooddrinks (name, imageURL, isMeal, apiID, ingredients, createdAt, updatedAt, ThemeId)
+VALUES ("Alice in Wonderland", "Take a journey down the rabbit hole for a mostly guarenteed good time.", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO fooddrinks (name, imageURL, isMeal, apiID, ingredients, createdAt, updatedAt, ThemeId)
+VALUES ("Sex and the City", "OMG BESTIES FOREVERRRR! *SQUEALS AND GROUP HUG*", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO fooddrinks (name, imageURL, isMeal, apiID, ingredients, createdAt, updatedAt, ThemeId)
+VALUES ("Fright Fest", "It'll be one hell of a hauntingly frightening night...", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO fooddrinks (name, imageURL, isMeal, apiID, ingredients, createdAt, updatedAt, ThemeId)
+VALUES ("Winter Wonderland", "Celebrate the season with friends and family", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO fooddrinks (name, imageURL, isMeal, apiID, ingredients, createdAt, updatedAt, ThemeId)
+VALUES ("Cinco de Drinko", "One tequila, two tequila, three tequila, floor.", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO fooddrinks (name, imageURL, isMeal, apiID, ingredients, createdAt, updatedAt, ThemeId)
+VALUES ("Prohibition", "Fight for your right to parrrtayyyy!", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO fooddrinks (name, imageURL, isMeal, apiID, ingredients, createdAt, updatedAt, ThemeId)
+VALUES ("A Night in Paris", "On se voit a paris?", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO fooddrinks (name, imageURL, isMeal, apiID, ingredients, createdAt, updatedAt, ThemeId)
+VALUES ("Impeachment Party", "About damn time.", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO fooddrinks (name, imageURL, isMeal, apiID, ingredients, createdAt, updatedAt, ThemeId)
+VALUES ("Redneck Yacht Club", "Featuring an appearance by recently impeached Donald Trump", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
