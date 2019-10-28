330f036d93d817b1fd045630e87c6b345d94b5b2
Nicole Marty <nmarty310@gmail.com>
2019-10-28 18:27:34 -0500

    fixed problem in the few columns missing instructions column and added button to add theme. still need to buttion to work.

--- public/js/index.js
+++ public/js/index.js
@@ -5,53 +5,53 @@ var $submitBtn = $("#submitTheme");
 
 // The API object contains methods for each kind of request we'll make
 var API = {
-  saveBuildTheme: function(newTheme) {
-    return $.ajax({
-      headers: {
-        "Content-Type": "application/json"
-      },
-      type: "POST",
-      url: "/api/themes",
-      data: JSON.stringify(newTheme)
-    });
-  },
-  getBuildTheme: function() {
-    return $.ajax({
-      url: "/api/themes",
-      type: "GET"
-    });
-  },
-  deleteBuildTheme: function(id) {
-    return $.ajax({
-      url: "/api/themes" + id,
-      type: "DELETE"
-    });
-  }
+    saveBuildTheme: function(newTheme) {
+        return $.ajax({
+            headers: {
+                "Content-Type": "application/json"
+            },
+            type: "POST",
+            url: "/api/themes",
+            data: JSON.stringify(newTheme)
+        });
+    },
+    getBuildTheme: function() {
+        return $.ajax({
+            url: "/api/themes",
+            type: "GET"
+        });
+    },
+    deleteBuildTheme: function(id) {
+        return $.ajax({
+            url: "/api/themes" + id,
+            type: "DELETE"
+        });
+    }
 };
 
 // handleFormSubmit is called whenever we submit a new newTheme
 // Save the new newTheme to the db and refresh the list
 var handleFormSubmit = function(event) {
-  event.preventDefault();
-  var newTheme = {
-    name: $newBuildThemeName.val().trim(),
-    description: $newBuildThemeDesc.val().trim()
-  };
-  if (!newTheme.name) {
-    alert("You must enter a theme name!");
-    return;
-  }
-  API.saveBuildTheme(newTheme)
-    .then(function(data) {
-      var queryStr = "?id=" + data.id + "&name=" + data.name;
-      window.location.href = "/build" + queryStr;
-    })
-    .catch(function(err) {
-      alert(err.responseText);
-    });
+    event.preventDefault();
+    var newTheme = {
+        name: $newBuildThemeName.val().trim(),
+        description: $newBuildThemeDesc.val().trim()
+    };
+    if (!newTheme.name) {
+        alert("You must enter a theme name!");
+        return;
+    }
+    API.saveBuildTheme(newTheme)
+        .then(function(data) {
+            var queryStr = "?id=" + data.id + "&name=" + data.name;
+            window.location.href = "/build" + queryStr;
+        })
+        .catch(function(err) {
+            alert(err.responseText);
+        });
 
-  $newBuildThemeName.val("");
-  $newBuildThemeDesc.val("");
+    $newBuildThemeName.val("");
+    $newBuildThemeDesc.val("");
 };
 
 // Add event listeners to the submit and delete buttons
--- public/js/themes.js
+++ public/js/themes.js
@@ -3,30 +3,30 @@ var dropdown = document.getElementsByClassName("dropdown-btn");
 var i;
 
 for (i = 0; i < dropdown.length; i++) {
-  dropdown[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var dropdownContent = this.nextElementSibling;
-    if (dropdownContent.style.display === "block") {
-      dropdownContent.style.display = "none";
-    } else {
-      dropdownContent.style.display = "block";
-    }
-  });
+    dropdown[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var dropdownContent = this.nextElementSibling;
+        if (dropdownContent.style.display === "block") {
+            dropdownContent.style.display = "none";
+        } else {
+            dropdownContent.style.display = "block";
+        }
+    });
 }
 
 var prevUrl = document.referrer;
 var themeId = prevUrl
-  .split("?")
-  .slice(1)
-  .toString()
-  .split("&")
-  .slice(0, 1)
-  .toString();
+    .split("?")
+    .slice(1)
+    .toString()
+    .split("&")
+    .slice(0, 1)
+    .toString();
 var strIndex = themeId.indexOf("=") + 1;
 themeId = themeId.slice(strIndex);
 
 if (themeId === "") {
-  $("#collapse1").addClass("show");
+    $("#collapse1").addClass("show");
 } else {
-  $("#collapse" + themeId).addClass("show");
+    $("#collapse" + themeId).addClass("show");
 }
