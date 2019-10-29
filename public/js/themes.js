9c21888c9c6aab712d7dc71e218a6cc6a8f25836
Ben Goligowski <51453763+btgoligowski24@users.noreply.github.com>
2019-10-28 19:24:47 -0500

    MVP done, we think...

--- public/js/themes.js
+++ public/js/themes.js
@@ -3,30 +3,40 @@ var dropdown = document.getElementsByClassName("dropdown-btn");
 var i;
 
 for (i = 0; i < dropdown.length; i++) {
-    dropdown[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var dropdownContent = this.nextElementSibling;
-        if (dropdownContent.style.display === "block") {
-            dropdownContent.style.display = "none";
-        } else {
-            dropdownContent.style.display = "block";
-        }
-    });
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
 }
 
 var prevUrl = document.referrer;
 var themeId = prevUrl
-    .split("?")
-    .slice(1)
-    .toString()
-    .split("&")
-    .slice(0, 1)
-    .toString();
+  .split("?")
+  .slice(1)
+  .toString()
+  .split("&")
+  .slice(0, 1)
+  .toString();
 var strIndex = themeId.indexOf("=") + 1;
 themeId = themeId.slice(strIndex);
 
 if (themeId === "") {
-    $("#collapse1").addClass("show");
+  $("#collapse1").addClass("show");
 } else {
-    $("#collapse" + themeId).addClass("show");
+  $("#collapse" + themeId).addClass("show");
 }
+
+function addToTheme() {
+  var themeId = $(this).attr("data-id");
+  var themeName = $(this).attr("data-name");
+  var queryStr = "?id=" + themeId + "&name=" + themeName;
+
+  window.location.href = "/build" + queryStr;
+}
+
+$("#accordionExample").on("click", ".addtotheme", addToTheme);
